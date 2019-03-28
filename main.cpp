5d48d945dcac0250e1803187d2dcb0aa8c2dc73f
Mariana Neto <32868723+bluediberry@users.noreply.github.com>
2019-03-28 20:04:34 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <cstring>
 #include <stdio.h>
+#include <unistd.h>
 
 
 using namespace std;
 
 const int MazeHeight = 18;
 const int MazeWidth = 18;
+
+void Up(int x, int y);
+void Down(int x, int y);
+void Left(int x, int y);
+void Right(int x, int y);
 
 
 char Maze[MazeHeight][MazeWidth] =
@@ -34,18 +40,20 @@ const int Wall = 1;
 const int Free = 0;
 const char Target = 'T';
 const char Robot = 'R';
+const char Passed = '*';
 
 
-class COORD{
+class COORD
+{
 public:
     int X;
     int Y;
-    COORD(int x = 0, int y = 0){X = x, Y = y;};
-    COORD(const COORD &coord) {X = coord.X, Y = coord.Y ;};
+    COORD(int x = 0, int y = 0) { X = x; Y = y; }
+    COORD(const COORD &coord) { X = coord.X; Y = coord.Y; }
 };
 
-COORD StartingPoint(10,3);
-COORD EndingPoint(3, 12);
+COORD StartingPoint(3,10);
+COORD EndingPoint(12, 3);
 
 
 void print_maze(){
@@ -64,48 +72,115 @@ void print_maze(){
             else if(Maze[i][j] == Robot){
                 cout << " R ";
             }
+            else if(Maze[i][j] == Passed){
+                cout << " * ";
+            }
         }
         
         cout << endl;
     }
 }
 
-void Up(int posx, int posy);
+bool Solve(int X, int Y){
+    
+    Maze[X][Y] = Robot;
+    
+    print_maze();
+    sleep(1);
+    
+    if (X == EndingPoint.X && Y == EndingPoint.Y)
+    {
+        return true;
+    }
+    else if (Y > 0 && Maze[X][Y - 1] == Free){
+        while (Maze[X][Y - 1] == Free || Maze[X][Y - 1]  == 'T') {
+            Maze[X][Y] = Free;
+            Maze[X][Y - 1] = 'R';
+            Y--;
+        }
+        return true;
+    }
+    else if (Y < MazeHeight && Maze[X][Y + 1] == Free){
+        while (Maze[X][Y + 1] == Free || Maze[X][Y + 1]  == 'T') {
+            Maze[X][Y] = Free;
+            Maze[X][Y + 1] = 'R';
+            Y++;
+        }
+        return true;
+    }
+    
+    else if (X > 0 && Maze[X + 1][Y] == Free){
+        while (Maze[X +1 ][Y] == Free || Maze[X + 1][Y]  == 'T') {
+            Maze[X][Y] = Free;
+            Maze[X + 1][Y] = 'R';
+            X++;
+        }
+        return true;
+        
+    }
+    else if (X < MazeWidth && Maze[X - 1][Y] == Free){
+        while (Maze[X - 1][Y] == Free || Maze[X - 1][Y]  == 'T') {
+            Maze[X][Y] = Free;
+            Maze[X - 1][Y] = 'R';
+            X--;
+        }
+        return true;
+    }
+    
+    Maze[X][Y] = Free;
+    
+    
+    //Maze[X][Y] = Robot;
+    print_maze();
+    sleep(1);
+    
+    return false;
+}
 
 int main() {
     
     print_maze();
+    
+    if (Solve(StartingPoint.X, StartingPoint.Y))
+    {
+        print_maze();
+    }
+    else
+    {
+        printf("Damn\n");
+    }
+    
     return 0;
 }
 
 void Up(int posx, int posy) {
-    while (Maze[posx][posy - 1] == 0 || Maze[posx][posy - 1] == 'T') {
-        Maze[posx][posy - 1] = 0;
-        Maze[posx][posy - 1] = 'A';
+    while (Maze[posx][posy - 1] == Free || Maze[posx][posy - 1] == 'T') {
+        Maze[posx][posy - 1] = Free;
+        Maze[posx][posy - 1] = 'R';
         posy = posy - 1;
     }
 }
 
 void Down(int posx, int posy) {
-    while (Maze[posx][posy + 1] == 0 || Maze[posx][posy + 1] == 'T') {
-        Maze[posx][posy + 1] = 0;
-        Maze[posx][posy + 1] = 'A';
+    while (Maze[posx][posy + 1] == Free || Maze[posx][posy + 1] == 'T') {
+        Maze[posx][posy + 1] = Free;
+        Maze[posx][posy + 1] = 'R';
         posy = posy + 1;
     }
 }
 
 void Left(int posx, int posy) {
-    while (Maze[posx - 1][posy] == 0 || Maze[posx - 1][posy] == 'T') {
-        Maze[posx - 1][posy] = 0;
-        Maze[posx - 1][posy] = 'A';
+    while (Maze[posx - 1][posy] == Free || Maze[posx - 1][posy] == 'T') {
+        Maze[posx - 1][posy] = Free;
+        Maze[posx - 1][posy] = 'R';
         posx = posx - 1;
     }
 }
 
 void Right(int posx, int posy) {
-    while (Maze[posx + 1][posy] == 0 || Maze[posx + 1][posy] == 'T') {
-        Maze[posx + 1][posy] = 0;
-        Maze[posx + 1][posy] = 'A';
+    while (Maze[posx + 1][posy] == Free || Maze[posx + 1][posy] == 'T') {
+        Maze[posx + 1][posy] = Free;
+        Maze[posx + 1][posy] = 'R';
         posx = posx + 1;
     }
 }
