5d74a0256a20d7238326237adc178621acfee581
Mariana Neto <32868723+bluediberry@users.noreply.github.com>
2019-03-29 13:04:31 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 using namespace std;
 
 const int MazeHeight = 18;
 const int MazeWidth = 18;
+
+void Up(int x, int y);
+void Down(int x, int y);
+void Left(int x, int y);
+void Right(int x, int y);
+
 
 char Maze[MazeHeight][MazeWidth] =
 {
@@ -35,7 +40,6 @@ const int Wall = 1;
 const int Free = 0;
 const char Target = 'T';
 const char Robot = 'R';
-const char Passed = '*';
 
 
 class COORD
@@ -68,9 +72,7 @@ void print_maze(){
             else if(Maze[i][j] == Robot){
                 cout << " R ";
             }
-            else if(Maze[i][j] == Passed){
-                cout << " * ";
-            }
+            
         }
         
         cout << endl;
@@ -78,19 +80,78 @@ void print_maze(){
 }
 
 vector<char> AvailableMoves(int x, int y) {
-	vector<char> moves;
-	if (Maze[x - 1][y] = Free)
-		moves.push_back('L');
-	if (Maze[x + 1][y] = Free)
-		moves.push_back('R');
-	if (Maze[x][y - 1] = Free)
-		moves.push_back('U');
-	if (Maze[x][y + 1] = Free)
-		moves.push_back('D');
-	return moves;
+    vector<char> moves;
+    if (Maze[x - 1][y] == Free)
+        moves.push_back('L');
+    if (Maze[x + 1][y] == Free)
+        moves.push_back('R');
+    if (Maze[x][y - 1] == Free)
+        moves.push_back('U');
+    if (Maze[x][y + 1] == Free)
+        moves.push_back('D');
+    return moves;
+}
+
+
+bool isNextToWall(int X, int Y){
+    return (Maze[Y - 1][X] == Wall || Maze[Y + 1][X] == Wall || Maze[Y][X - 1] == Wall || Maze[Y][X - 1] == Wall);
+}
+
+
+bool Solve(int X, int Y)
+{
+    // Make the move (if it's wrong, we will backtrack later.
+    Maze[Y][X] = Robot;
+    
+    // If you want progressive update, uncomment these lines...
+    if(isNextToWall(X, Y)){
+        print_maze();
+        sleep(1);}
+    
+    // Check if we have reached our goal.
+    if (X == EndingPoint.X && Y == EndingPoint.Y)
+    {
+        exit(0);
+    }
+    
+    // Recursively search for our goal.
+    if (Y > 0 && (Maze[Y - 1][X] == Free || Maze[Y - 1][X] == Target) && Solve(X, Y - 1))
+    {
+        return true;
+    }
+    if (Y < MazeHeight && (Maze[Y + 1][X] == Free || Maze[Y + 1][X] == Target) && Solve(X, Y + 1))
+    {
+        return true;
+    }
+    if (X > 0 && (Maze[Y][X - 1] == Free || Maze[Y][X - 1] == Target) && Solve(X - 1, Y))
+    {
+        return true;
+    }
+    if (X < MazeWidth && (Maze[Y][X + 1] == Free || Maze[Y][X + 1] == Target) && Solve(X + 1, Y))
+    {
+        return true;
+    }
+    
+    // Otherwise we need to backtrack and find another solution.
+    Maze[Y][X] = Free;
+    
+    print_maze();
+    sleep(1);
+    return false;
 }
 
 int main() {
+    
+    print_maze();
+    
+    if (Solve(StartingPoint.X, StartingPoint.Y))
+    {
+        print_maze();
+    }
+    else
+    {
+        printf("Damn\n");
+    }
     
     
     return 0;
@@ -127,3 +188,5 @@ void Right(int posx, int posy) {
         posx = posx + 1;
     }
 }
+
+
